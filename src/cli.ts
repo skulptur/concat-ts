7232c2194bc1f9b4475640a38de1d89fc8987a23
skulptur <g.spredemann@gmail.com>
2023-03-21 14:15:46 +0100

    fix buid

--- src/cli.ts
+++ src/cli.ts
@@ -1,19 +1,18 @@
 #!/usr/bin/env node
 
-import program from "commander";
+import { program } from "commander";
 import { concatTs } from "./index";
 
 program
-  .version("0.1.0")
   .option("-i, --input <path>", "Input entry path")
   .option("-o, --output <path>", "Output path")
   .parse(process.argv);
 
-console.log(program.opts());
+const options = program.opts();
 
 console.log(
   concatTs({
-    input: program.input,
-    output: program.output,
+    input: options.input,
+    output: options.output,
   })
 );
